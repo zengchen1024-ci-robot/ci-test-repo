60e940a57653973ce603eb432c327ee04dd49912
zengchen1024-ci-robot <chenzeng_ci_robot@163.com>
2020-03-18 18:04:59 +0800

    test99

---
+++ t.sh
@@ -0,0 +1,15 @@
+if [$# -ne 1]; then
+  echo "input branch number"
+  exit 0
+fi
+
+branch="test$1"
+file="test$1.tf"
+message="test$1"
+
+git checkout master
+git checkout -b $branch
+touch $file
+git add .
+git commit -m "$message"
+git log
